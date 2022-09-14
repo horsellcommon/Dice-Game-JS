2fdc16fa6d95d6664c50eb286e4ecf63b3dfb414
horsellcommon <crawfordj716@gmail.com>
2022-09-14 12:30:13 +0100

    Added win/lose and if statements

--- notes.md
+++ notes.md
@@ -8,3 +8,5 @@ TODO:
 - Make button change src of dice image (math.random???)
 - Create variable called score that adds up
 - Create element below score that tracks score
+- Once score is past 20, reset score to 0 and replace element that tracks with the text "YOU WIN"
+- If 1 is rolled, reset score to 0 and replace element that tracks with the text "YOU LOSE"
--- script.js
+++ script.js
@@ -1,31 +1,56 @@
 const button = document.getElementById("button")
 const diceImage = document.getElementById("diceImage")
+const scoreElement = document.getElementById("score")
 let score = 0
+scoreElement.textContent = 0
 console.log(diceImage)
+
+const elementUpdater = () => {
+    scoreElement.textContent = score
+}
 
 button.addEventListener("click", () => {
     const randValue = Math.round(Math.random()*6)
-    if (randValue == 1){ //if 1 is rolled - reset score
+    if (randValue <= 1){
         diceImage.src="img/dice1.png"
-        score = score + randValue
+        score = 0
+        scoreElement.textContent = "YOU LOSE"
     } else if (randValue == 2){
         diceImage.src="img/dice2.png"
         score = score + randValue
+        elementUpdater()
+        if (score >= 20){
+            scoreElement.textContent = "YOU WIN"
+        }
     } else if (randValue == 3){
         diceImage.src="img/dice3.png"
         score = score + randValue
+        elementUpdater()
+        if (score >= 20){
+            scoreElement.textContent = "YOU WIN"
+        }
     } else if (randValue == 4){
         diceImage.src="img/dice4.png"
         score = score + randValue
+        elementUpdater()
+        if (score >= 20){
+            scoreElement.textContent = "YOU WIN"
+        }
     } else if (randValue == 5){
         diceImage.src="img/dice5.png"
         score = score + randValue
+        elementUpdater()
+        if (score >= 20){
+            scoreElement.textContent = "YOU WIN"
+        }
     } else if (randValue == 6){
         diceImage.src="img/dice6.png"
         score = score + randValue
+        elementUpdater()
+        if (score >= 20){
+            scoreElement.textContent = "YOU WIN"
+        }
     }
- 
-
     console.log(randValue)
     console.log(score)
 })
