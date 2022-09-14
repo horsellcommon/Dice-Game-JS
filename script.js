c09d291085b1ae2e9425310e8da83e2b711d38f7
horsellcommon <crawfordj716@gmail.com>
2022-09-14 16:52:35 +0100

    Score holders

--- script.js
+++ script.js
@@ -7,10 +7,16 @@ const p2diceImage = document.getElementById("p2-diceImage")
 const p2scoreElement = document.getElementById("p2-score")
 const p2winLoseElement = document.getElementById("p2-winlose")
 const newGame = document.getElementById("newgame")
+const holdScore = document.getElementById("hold")
+const p2holdScore = document.getElementById("p2-hold")
+const totalScore = document.getElementById("totalscore")
+const p2totalScore = document.getElementById("p2totalscore")
 let score = 0
 let p2score = 0
+totalScore.textContent = 0
 scoreElement.textContent = 0
 p2scoreElement.textContent = 0
+p2button.disabled = true
 
 const winUpdater = () => {
     winLoseElement.textContent = "YOU WIN"
@@ -32,14 +38,62 @@ const p2elementUpdater = () => {
 }
 
 newGame.addEventListener("click", () => {
+    if (score >= 20){
+        button.disabled = true
+        p2button.disabled = false
+    } else if (p2score >= 20){
+        button.disabled = false
+        p2button.disabled = true
+    } else if (score < 20 && p2score < 20){
+        button.disabled = false
+        p2button.disabled = true
+    }
     score = 0
     p2score = 0
     diceImage.src=""
     p2diceImage.src=""
     scoreElement.textContent="0"
+    totalScore.textContent="0"
     p2scoreElement.textContent="0"
     winLoseElement.textContent=""
     p2winLoseElement.textContent=""
+})
+
+let accumulatedScore = 0
+let p2accumulatedScore = 0
+holdScore.addEventListener("click", () => {
+    button.disabled = true
+    p2button.disabled = false
+    accumulatedScore = accumulatedScore+score
+    totalScore.textContent = accumulatedScore
+    score = 0
+    scoreElement.textContent=0
+    if (accumulatedScore >= 20){
+        elementUpdater()
+        winUpdater()
+        p2lossUpdater()
+        button.disabled = true
+        p2button.disabled = true
+        holdScore.disabled = true
+        p2holdScore.disabled = true
+    }
+})
+p2holdScore.addEventListener("click", () => {
+    p2button.disabled = true
+    button.disabled = false
+    p2accumulatedScore = p2accumulatedScore+p2score
+    p2totalScore.textContent = p2accumulatedScore
+    p2score = 0
+    p2scoreElement.textContent=0
+    if (p2accumulatedScore >= 20){
+        p2elementUpdater()
+        p2winUpdater()
+        lossUpdater()
+        button.disabled = true
+        p2button.disabled = true
+        holdScore.disabled = true
+        p2holdScore.disabled = true
+    }
 })
 
 button.addEventListener("click", () => {
@@ -47,11 +101,19 @@ button.addEventListener("click", () => {
     const randValue = Math.round(Math.random()*6)
     if (randValue <= 1){
         diceImage.src=""
-        elementUpdater()
-        lossUpdater()
-        p2winUpdater()
         score = 0
-        p2score = 0
+        button.disabled = true
+        p2button.disabled = false
+        elementUpdater()
+        if (score >= 20){
+            elementUpdater()
+            winUpdater()
+            p2lossUpdater()
+            button.disabled = true
+            p2button.disabled = true
+            holdScore.disabled = true
+            p2holdScore.disabled = true
+        }
     } else if (randValue == 2){
         diceImage.src="img/dice2.png"
         score = score + randValue
@@ -59,11 +121,11 @@ button.addEventListener("click", () => {
         if (score >= 20){
             elementUpdater()
             winUpdater()
-            diceImage.src=""
-            score = 0
             p2lossUpdater()
-            p2diceImage.src=""
-            p2score = 0
+            button.disabled = true
+            p2button.disabled = true
+            holdScore.disabled = true
+            p2holdScore.disabled = true
         }
     } else if (randValue == 3){
         diceImage.src="img/dice3.png"
@@ -72,11 +134,11 @@ button.addEventListener("click", () => {
         if (score >= 20){
             elementUpdater()
             winUpdater()
-            diceImage.src=""
-            score = 0
             p2lossUpdater()
-            p2diceImage.src=""
-            p2score = 0
+            button.disabled = true
+            p2button.disabled = true
+            holdScore.disabled = true
+            p2holdScore.disabled = true
         }
     } else if (randValue == 4){
         diceImage.src="img/dice4.png"
@@ -85,11 +147,11 @@ button.addEventListener("click", () => {
         if (score >= 20){
             elementUpdater()
             winUpdater()
-            diceImage.src=""
-            score = 0
             p2lossUpdater()
-            p2diceImage.src=""
-            p2score = 0
+            button.disabled = true
+            p2button.disabled = true
+            holdScore.disabled = true
+            p2holdScore.disabled = true
         }
     } else if (randValue == 5){
         diceImage.src="img/dice5.png"
@@ -98,11 +160,11 @@ button.addEventListener("click", () => {
         if (score >= 20){
             elementUpdater()
             winUpdater()
-            diceImage.src=""
-            score = 0
             p2lossUpdater()
-            p2diceImage.src=""
-            p2score = 0
+            button.disabled = true
+            p2button.disabled = true
+            holdScore.disabled = true
+            p2holdScore.disabled = true
         }
     } else if (randValue == 6){
         diceImage.src="img/dice6.png"
@@ -111,11 +173,11 @@ button.addEventListener("click", () => {
         if (score >= 20){
             elementUpdater()
             winUpdater()
-            diceImage.src=""
-            score = 0
             p2lossUpdater()
-            p2diceImage.src=""
-            p2score = 0
+            button.disabled = true
+            p2button.disabled = true
+            holdScore.disabled = true
+            p2holdScore.disabled = true
         }
     }
     console.log(randValue)
@@ -127,12 +189,19 @@ p2button.addEventListener("click", () => {
     const p2randValue = Math.round(Math.random()*6)
     if (p2randValue <= 1){
         p2diceImage.src=""
-        p2elementUpdater()
-        p2lossUpdater()
+        p2button.disabled = true
+        button.disabled = false
         p2score = 0
-        winUpdater()
-        diceImage.src=""
-        score = 0
+        p2elementUpdater()
+        if (p2score >= 20){
+            p2elementUpdater()
+            p2winUpdater()
+            lossUpdater()
+            button.disabled = true
+            p2button.disabled = true
+            holdScore.disabled = true
+            p2holdScore.disabled = true
+        }
     } else if (p2randValue == 2){
         p2diceImage.src="img/dice2.png"
         p2score = p2score + p2randValue
@@ -140,11 +209,11 @@ p2button.addEventListener("click", () => {
         if (p2score >= 20){
             p2elementUpdater()
             p2winUpdater()
-            p2diceImage.src=""
-            p2score = 0
             lossUpdater()
-            diceImage.src=""
-            score = 0
+            button.disabled = true
+            p2button.disabled = true
+            holdScore.disabled = true
+            p2holdScore.disabled = true
         }
     } else if (p2randValue == 3){
         p2diceImage.src="img/dice3.png"
@@ -153,11 +222,11 @@ p2button.addEventListener("click", () => {
         if (p2score >= 20){
             p2elementUpdater()
             p2winUpdater()
-            p2diceImage.src=""
-            p2score = 0
             lossUpdater()
-            diceImage.src=""
-            score = 0
+            button.disabled = true
+            p2button.disabled = true
+            holdScore.disabled = true
+            p2holdScore.disabled = true
         }
     } else if (p2randValue == 4){
         p2diceImage.src="img/dice4.png"
@@ -166,11 +235,11 @@ p2button.addEventListener("click", () => {
         if (p2score >= 20){
             p2elementUpdater()
             p2winUpdater()
-            p2diceImage.src=""
-            p2score = 0
             lossUpdater()
-            diceImage.src=""
-            score = 0
+            button.disabled = true
+            p2button.disabled = true
+            holdScore.disabled = true
+            p2holdScore.disabled = true
         }
     } else if (p2randValue == 5){
         p2diceImage.src="img/dice5.png"
@@ -179,11 +248,11 @@ p2button.addEventListener("click", () => {
         if (p2score >= 20){
             p2elementUpdater()
             p2winUpdater()
-            p2diceImage.src=""
-            p2score = 0
             lossUpdater()
-            diceImage.src=""
-            score = 0
+            button.disabled = true
+            p2button.disabled = true
+            holdScore.disabled = true
+            p2holdScore.disabled = true
         }
     } else if (p2randValue == 6){
         p2diceImage.src="img/dice6.png"
@@ -192,11 +261,11 @@ p2button.addEventListener("click", () => {
         if (p2score >= 20){
             p2elementUpdater()
             p2winUpdater()
-            p2diceImage.src=""
-            p2score = 0
             lossUpdater()
-            diceImage.src=""
-            score = 0
+            button.disabled = true
+            p2button.disabled = true
+            holdScore.disabled = true
+            p2holdScore.disabled = true
         }
     }
     console.log(p2randValue)
