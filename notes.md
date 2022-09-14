d41cdabec88ebc841e5f8f15b47642473f6d7672
horsellcommon <crawfordj716@gmail.com>
2022-09-14 12:10:41 +0100

    Added score tracker variable

--- notes.md
+++ notes.md
@@ -6,4 +6,5 @@ TODO:
 - Make button work!!!
 - Give dice image id
 - Make button change src of dice image (math.random???)
+- Create variable called score that adds up
 - Create element below score that tracks score
--- script.js
+++ script.js
@@ -1,22 +1,31 @@
 const button = document.getElementById("button")
 const diceImage = document.getElementById("diceImage")
+let score = 0
 console.log(diceImage)
 
 button.addEventListener("click", () => {
-    console.log("Does it work????")
     const randValue = Math.round(Math.random()*6)
-    console.log(randValue)
-    if (randValue == 1){
+    if (randValue == 1){ //if 1 is rolled - reset score
         diceImage.src="img/dice1.png"
+        score = score + randValue
     } else if (randValue == 2){
         diceImage.src="img/dice2.png"
+        score = score + randValue
     } else if (randValue == 3){
         diceImage.src="img/dice3.png"
+        score = score + randValue
     } else if (randValue == 4){
         diceImage.src="img/dice4.png"
+        score = score + randValue
     } else if (randValue == 5){
         diceImage.src="img/dice5.png"
+        score = score + randValue
     } else if (randValue == 6){
         diceImage.src="img/dice6.png"
+        score = score + randValue
     }
+ 
+
+    console.log(randValue)
+    console.log(score)
 })
